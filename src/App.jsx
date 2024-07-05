d74a4eb69f9a1ddd7a33bdcba7b6081e6a601330
vinishasinha <159924188+vinishasinha@users.noreply.github.com>
2024-07-05 14:58:13 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react'
+import Sidebar from './components/Sidebar'
+import Player from './components/Player'
+import Display from './components/Display'
+import { PlayerContext } from './context/PlayerContext'
+
+const App = () => {
+  const {audioRef,track}=useContext(PlayerContext);
+  return (
+    <div className='h-screen bg-black'> 
+    <div className='h-[90%] flex'>
+      <Sidebar/>
+      <Display/>
+    </div>
+    <Player/>
+    <audio ref={audioRef} src={track.file} preload='auto'></audio>
+    </div>
+  )
+}
+
+export default App
