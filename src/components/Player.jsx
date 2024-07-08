74283cdbb6bc8394a66905bf6bc41224dc27686d
vinishasinha <159924188+vinishasinha@users.noreply.github.com>
2024-07-08 17:20:21 +0530

    Update Player.jsx

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,7 +3,7 @@ import {assets} from '../assets/assets'
 import { PlayerContext } from '../context/PlayerContext';
 
 const Player = () => {
-    const {track,seekBar,seekBg,playStatus,play,pause,time}=useContext(PlayerContext);
+    const {track,seekBar,seekBg,playStatus,play,pause,time,previous,next,seekSong}=useContext(PlayerContext);
   return (
     <div className='h-[10%] bg-black flex justify-between items-center text-white px-4'>
         <div className='hidden lg:flex items-center gap-4 '>
@@ -15,8 +15,8 @@ const Player = () => {
         </div>
         <div className='flex flex-col items-center gap-1 m-auto'>
             <div className='flex gap-4'>
-                <img className='w-4 cursor-pointer' src={assets.shuffle_icon} alt="" />
-                <img className='w-4 cursor-pointer' src={assets.prev_icon} alt="" />
+                <img  className='w-4 cursor-pointer' src={assets.shuffle_icon} alt="" />
+                <img onClick={previous} className='w-4 cursor-pointer' src={assets.prev_icon} alt="" />
                 {playStatus ?
                  <img onClick={pause} className='w-4 cursor-pointer' src={assets.pause_icon} alt="" /> 
                 :
@@ -24,13 +24,13 @@ const Player = () => {
                 }
                 
                
-                <img className='w-4 cursor-pointer' src={assets.next_icon} alt="" />
+                <img onClick={next} className='w-4 cursor-pointer' src={assets.next_icon} alt="" />
                 <img className='w-4 cursor-pointer' src={assets.loop_icon} alt="" />
 
             </div>
             <div className='flex items-center gap-5'>
                 <p>{time.currentTime.minute}:{time.currentTime.second}</p>
-                <div ref={seekBg} className='w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer '>
+                <div ref={seekBg} onClick={seekSong} className='w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer '>
                     <hr ref={seekBar} className='h-1 border-none w-0 bg-green-800 rounded-full' />
                 </div>
                 <p>{time.totalTime.minute}:{time.totalTime.second}</p>
