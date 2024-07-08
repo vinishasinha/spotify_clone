993fce2636a9f662c70a2786b1a8f50f12165b67
vinishasinha <159924188+vinishasinha@users.noreply.github.com>
2024-07-08 17:21:37 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { assets } from '../assets/assets';
+import { useNavigate } from 'react-router-dom';
 
 const Sidebar = () => {
+    const navigate=useNavigate();
   return (
+    
     <div className='w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex'>
         <div className='bg-[#121212] h-[20%] rounded flex flex-col justify-around'>
         <div className='flex item-center gap-3 pl-8 cursor-pointer'>
                 <img className="w-6" src={assets.spotify_logo} alt="" />
                 <p className='font-bold'>Spotify</p>
             </div>
-            <div className='flex item-center gap-3 pl-8 cursor-pointer mt-3'>
+            <div onClick={()=>navigate('/')} className='flex item-center gap-3 pl-8 cursor-pointer mt-3'>
                 <img className="w-6" src={assets.home_icon} alt="" />
                 <p className='font-bold'>Home</p>
             </div>
