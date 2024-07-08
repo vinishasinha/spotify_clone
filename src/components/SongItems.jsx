59ad46e4cbfaf92d2ec733709c839a6738e63b6d
vinishasinha <159924188+vinishasinha@users.noreply.github.com>
2024-07-08 17:22:30 +0530

    Update SongItems.jsx

--- src/components/SongItems.jsx
+++ src/components/SongItems.jsx
@@ -1,17 +1,16 @@
 import React, { useContext } from 'react'
 import { PlayerContext } from '../context/PlayerContext'
 
-const SongItems = ({name,image,desc,id}) => {
-  
-   const {playWithId}=useContext(PlayerContext);
+const SongItem = ({name,image,desc,id}) => {
+  const {playWithId}= useContext(PlayerContext)
 
   return (
-    <div  className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
-        <img className='rounded' src={image} alt="" />
-       <p className='font-bold mt-2 mb-1'>{name}</p>
-       <p className='text-slate-200 text-sm'>{desc}</p>
+    <div onClick={()=>playWithId(id)} className='min-w-[180px] p-2 px-3 rounded curs  or-pointer hover:bg-[#ffffff26]'>
+      <img className='rounded' src={image} alt="" />
+      <p className='font-bold mt-2 mb-1'>{name} </p>
+      <p className='text-slate-200 text-sm'>{desc}</p>
     </div>
   )
 }
 
-export default SongItems
+export default SongItem
