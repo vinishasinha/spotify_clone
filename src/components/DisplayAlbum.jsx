24427090db18873f2ba1b319f2ba546ffe910f35
vinishasinha <159924188+vinishasinha@users.noreply.github.com>
2024-07-08 17:19:55 +0530

    Update DisplayAlbum.jsx

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import NavBar from './Navbar'
 import {useParams} from 'react-router-dom'
 import { albumsData, assets, songsData } from '../assets/assets';
+import { PlayerContext } from '../context/PlayerContext';
 
 const DisplayAlbum = () => {
   const {id}=useParams();
   const albumData= albumsData[id]; 
+  const {playWithId}=useContext(PlayerContext)
+  
   
   return (
     <>
@@ -35,7 +38,7 @@ const DisplayAlbum = () => {
     <hr />
     {
       songsData.map((item,index)=>(
-        <div key={index} className='grid grid-cols-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer'>
+        <div onClick={()=>playWithId(item.id)} key={index} className='grid grid-cols-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer'>
       <p className='text-white'>
         <b className='mr-4 text-[#a7a7a7]'>{index+1}</b>
         <img className='inline w-10 mr-5' src={item.image} alt="" />
